19f444c84b3c60ba1c8cea3416055fdc70b9b3d8
Damian Bravo <Dbravo1005@gmail.com>
2023-05-03 22:05:16 -0400

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+    return upperCaseCustomerName
+}
+
+
+var bestCustomer;
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+    return setBestCustomer
+}
+
+function overwriteBestCustomer(){
+     bestCustomer = 'maybe bob'
+    return overwriteBestCustomer
+}
+
+const leastFavoriteCustomer = 'Dylan'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Damian"
+    return changeLeastFavoriteCustomer
+}
